b9a4e27469a7b066918b8160a5e4aaac264acf46
LeBakaSable <hugogole.ito@gmail.com>
2024-05-01 09:40:21 +0200

    feat: show text into cells

--- plug.c
+++ plug.c
@@ -7,42 +7,19 @@
 #include <raymath.h>
 #include "common.h"
 
+#define FONT_SIZE 52
+
 #define COLOR_BASE   GetColor(0x1e1e2eff)
 #define COLOR_RED    GetColor(0xf38ba8ff)
 #define COLOR_YELLOW GetColor(0xf9e2afff)
+#define COLOR_BLUE   GetColor(0x89b4faff)
+#define COLOR_TEXT   GetColor(0xcdd6f4ff)
 
 typedef struct {
   size_t i;
   float duration;
   bool loop;
 } Animation;
-
-typedef struct {
-  Color background;
-  Animation a;
-} Plug;
-
-static Plug *p = NULL;
-
-void plug_init(void)
-{
-  TraceLog(LOG_INFO, "Initializing plugin");
-  p = malloc(sizeof(*p));
-  assert(p != NULL);
-  memset(p, 0, sizeof(*p));
-  p->background = COLOR_BASE;
-  p->a.loop = true;
-}
-
-void *plug_pre_reload(void)
-{
-  return p;
-}
-
-void plug_post_reload(void *state)
-{
-  p = state;
-}
 
 typedef struct {
   float from;
@@ -84,39 +61,83 @@ void animation_update(Animation *a, Keyframe *kfs, size_t kfs_count)
   }
 }
 
+typedef struct {
+  Animation a;
+  Font font;
+} Plug;
+
+static Plug *p = NULL;
+
+void plug_init(void)
+{
+  TraceLog(LOG_INFO, "Initializing plugin");
+  p = malloc(sizeof(*p));
+  assert(p != NULL);
+  memset(p, 0, sizeof(*p));
+  p->a.loop = true;
+  p->font = LoadFontEx("fonts/ttf/JetBrainsMono-Regular.ttf", FONT_SIZE, 0, 250);
+}
+
+void *plug_pre_reload(void)
+{
+  return p;
+}
+
+void plug_post_reload(void *state)
+{
+  p = state;
+}
+
 void plug_update(void)
 {
-  Keyframe kfs[] = {
-    {
-      .from = 0.0,
-      .to = 0.250,
-      .duration = 0.250,
-    },
-    {
-      .from = 0.25,
-      .to = 0.25,
-      .duration = 0.5,
-    },
-    {
-      .from = 0.25,
-      .to = 1.0,
-      .duration = 0.5,
-    },
-  };
-
+  float rw = 100.0;
+  float rh = 100.0;
+  Vector2 cell_size = {rw, rh};
+  float pad = rw*0.15;
   float w = GetScreenWidth();
   float h = GetScreenHeight();
+
+  size_t offset = 7;
+  Keyframe kfs[] = {
+    {.from = w/2 - rw/2 - (offset + 0)*(rw + pad), .to = w/2 - rw/2 - (offset + 0)*(rw + pad), .duration = 0.5},
+    {.from = w/2 - rw/2 - (offset + 0)*(rw + pad), .to = w/2 - rw/2 - (offset + 1)*(rw + pad), .duration = 0.5},
+    {.from = w/2 - rw/2 - (offset + 1)*(rw + pad), .to = w/2 - rw/2 - (offset + 1)*(rw + pad), .duration = 0.5},
+    {.from = w/2 - rw/2 - (offset + 1)*(rw + pad), .to = w/2 - rw/2 - (offset + 2)*(rw + pad), .duration = 0.5},
+    {.from = w/2 - rw/2 - (offset + 2)*(rw + pad), .to = w/2 - rw/2 - (offset + 2)*(rw + pad), .duration = 0.5},
+    {.from = w/2 - rw/2 - (offset + 2)*(rw + pad), .to = w/2 - rw/2 - (offset + 3)*(rw + pad), .duration = 0.5},
+    //{.from = w/2 - rw/2 - (offset + 3)*(rw + pad), .to = w/2 - rw/2 - (offset + 3)*(rw + pad), .duration = 0.5},
+    //{.from = w/2 - rw/2 - (offset + 3)*(rw + pad), .to = w/2 - rw/2 - (offset + 0)*(rw + pad), .duration = 0.5},
+  };
+
+#if 1
+  Color cell_color = COLOR_TEXT;
+  Color head_color = COLOR_BLUE;
+  Color background_color = COLOR_BASE;
+#else
+  Color cell_color = COLOR_BASE;
+  Color head_color = COLOR_BLUE;
+  Color background_color = COLOR_TEXT;
+#endif
 
   BeginDrawing();
   animation_update(&p->a, kfs, ARRAY_LEN(kfs));
   float t = animation_value(p->a, kfs, ARRAY_LEN(kfs));
-  ClearBackground(a->background);
-  float rw = 100.0;
-  float rh = 100.0;
-  float pad = rw*0.15;
-  Color cell_color = COLOR_RED;
-  for (size_t i = 0; i < 10; ++i) {
-    DrawRectangle(i*(rw + pad) - w*t, h/2 - rh/2, rw, rh, cell_color);
+  ClearBackground(background_color);
+  for (size_t i = 0; i < 200; ++i) {
+    Rectangle rec = {
+      .x = i*(rw + pad) + t,
+      .y = h/2 - rh/2,
+      .width = rw,
+      .height = rh,
+    };
+    DrawRectangleRec(rec, cell_color);
+
+    const char *text = "0";
+    Vector2 text_size = MeasureTextEx(p->font, text, FONT_SIZE, 0);
+    Vector2 position = { .x = rec.x, .y = rec.y };
+    position = Vector2Add(position, Vector2Scale(cell_size, 0.5));
+    position = Vector2Subtract(position, Vector2Scale(text_size, 0.5));
+    DrawTextEx(p->font, text, position, FONT_SIZE, 0, background_color);
   }
 
   float head_thick = 20.0;
@@ -126,7 +147,7 @@ void plug_update(void)
   };
   rec.x = w/2 - rec.width/2;
   rec.y = h/2 - rec.height/2;
-  DrawRectangleLinesEx(rec, head_thick, COLOR_YELLOW);
+  DrawRectangleLinesEx(rec, head_thick, head_color);
 
   EndDrawing();
 }
